65cbb3c9be374230b76de8b7596e648afaecd219
Lucas Noan <noanpires@gmail.com>
2023-12-12 17:44:20 -0300

    att

---
+++ Desafios/ex005.py
@@ -0,0 +1,2 @@
+n = int(input('Digite um número: '))
+print('O número sucessor de {} é {} e seu antecessor é {}'.format(n, n+1,n-1))
---
+++ Desafios/ex008.py
@@ -0,0 +1,4 @@
+metros = int(input('Digite um valor em Metros: '))
+centimetros = metros * 100
+milimetros = metros * 1000
+print('O valor de {} metros em centímetros é {} já em milímetros é {}'.format(metros, centimetros, milimetros))
