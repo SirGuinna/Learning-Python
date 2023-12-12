0fafd8a5874dc88f18055905552202b4996e3abf
Lucas Noan <noanpires@gmail.com>
2023-12-12 16:59:47 -0300

    Create aula07.py

---
+++ Aulas/aula07.py
@@ -0,0 +1,9 @@
+n1 = int(input('Um valor: '))
+n2 = int(input('Outro valor: '))
+soma = n1 + n2
+multiplic = n1 * n2
+divis = n1 / n2
+divisinteira = n1 // n2
+exponenc = n1 ** n2
+print('A soma é {}, o produto é {} e a divisão é {:.3f}'.format(soma, multiplic, divis))
+print('Divisão inteira {} e potência {}'.format(divisinteira, exponenc))
