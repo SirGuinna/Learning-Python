9043169a5c989201620053370e358542bf68f6a0
Lucas Noan <noanpires@gmail.com>
2023-12-12 15:18:46 -0300

    att

--- Exercícios/ex003.py
+++ Exercícios/ex003.py
@@ -1,4 +1,4 @@
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite mais um número: '))
 soma = n1 + n2
-print('A soma vale ', soma)
+print('A soma entre {} e {} resulta em {} '.format(n1, n2, soma))
