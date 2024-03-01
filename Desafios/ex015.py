f90ce07d79e9f25d03117af61185764d7fa83d11
Lucas Noan <noanpires@gmail.com>
2024-03-01 14:16:54 -0300

    correção

--- Desafios/ex015.py
+++ Desafios/ex015.py
@@ -1,4 +1,4 @@
-dias = float(input('Quantos dias o veículo esteve alugado? '))
-km = int(input('Quantos Kilometros foram percorridos no total? '))
-d = 60 * dias
-pk = 0.15 * km
+dias = int(input('Quantos dias o veículo esteve alugado? '))
+km = float(input('Quantos Kilometros foram percorridos no total? '))
+total = (60 * dias) + (0.15 * km)
+print('O total a pagar é R${:.2f}'.format(total))
